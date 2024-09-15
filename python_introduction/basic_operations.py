2207428be8694e13073c0d2e9242ef5146b082a7
ZEZE1020 <ogembogodfrey75@gmail.com>
2024-09-15 05:33:32 -0700

    BasicOperations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = int(10)
+number2 = int(5)
+
+Addition = (number1 + number2)
+Subtraction = (number1 - number2)
+Multiplication = (number1 * number2)
+
+print (f"Addition of {number1} and {number2} is {Addition}")
+print (f"Subtraction of {number1} and {number2} is {Subtraction}")
+print (f"Multiplication of {number1} and {number2} is {Multiplication}")

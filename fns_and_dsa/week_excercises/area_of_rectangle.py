795feb012db9339c793544bb1d47b2c58ea0336e
ogembo <60712273+ZEZE1020@users.noreply.github.com>
2024-09-25 22:00:41 +0300

    E

---
+++ fns_and_dsa/week_excercises/area_of_rectangle.py
@@ -0,0 +1,9 @@
+def calculate_area():
+    length = float(input("Enter the length of the rectangle:  "))
+    width = float(input("Enter the width of the rectangle:  "))
+    area = length * width
+    print(f"The area of the rectangle with {length} length and {width} width is {area} square units.")
+
+# calling the function:
+calculate_area()
+

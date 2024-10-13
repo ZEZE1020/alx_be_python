2b05af88e9a0f8008268346fa9fb3b42f6c74ddd
ogembo <60712273+ZEZE1020@users.noreply.github.com>
2024-10-13 18:21:40 +0300

    Libsys

--- oop/library_system.py
+++ oop/library_system.py
@@ -14,7 +14,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):

dd4f30fee2a0655ae2f54a4ee72a4c9ffefd8a94
ogembo <60712273+ZEZE1020@users.noreply.github.com>
2024-10-13 17:08:18 +0300

    foldercreated

---
+++ oop/book_class.py
@@ -0,0 +1,17 @@
+# book_class.py
+
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+        print(f"Book '{self.title}' has been created.")
+
+    def __del__(self):
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
